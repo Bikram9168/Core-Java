701a7d20cc8657e3b7fb11e8ff0e30797bba579c
Bikram Kundu <bikramkundu20122005@gmail.com>
2026-08-22 21:11:59 +0530

    Add some bit operations on Java program

--- All_Basics/Arithmetic.java
+++ All_Basics/Arithmetic.java
@@ -1,7 +1,11 @@
 public class Arithmetic {
     public static void main(String[] args) {
-        int num = 10;
-        System.out.print("The number is :"+ num);
-        
+        int a = 2;
+        int b = 3;
+        int c = a & b;
+        int d = a | b;
+        int e = a ^ b;
+        int f = ~a;
+        System.out.println(c + " , "+d+" , "+e+" , "+f);
     }
 }
