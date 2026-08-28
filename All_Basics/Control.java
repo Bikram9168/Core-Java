0e51a46ac186b8fc3f9fd91d575eebeb6f4ead5d
Bikram Kundu <bikramkundu20122005@gmail.com>
2026-08-28 11:50:49 +0530

    Add New File

---
+++ All_Basics/Control.java
@@ -0,0 +1,20 @@
+public class Control {
+    public static void main(String[] args) {
+        // Switch statements
+        int i=3;
+        switch (i) {
+            case 1:
+                System.out.println("i is 1");
+                break;
+            case 2:
+                System.out.println("i is 2");
+                break;
+            case 3:
+                System.out.println("i is 3");
+                break;
+            default:
+                System.out.println("Default Statement");
+                break;
+        }
+    }
+}
