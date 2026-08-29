ebe5781335c8d7edfc06779049c6daff2ad802da
Bikram Kundu <bikramkundu20122005@gmail.com>
2026-08-29 11:09:26 +0530

    Change in Code

--- All_Basics/Control.java
+++ All_Basics/Control.java
@@ -1,20 +1,26 @@
 public class Control {
     public static void main(String[] args) {
         // Switch statements
-        int i=3;
-        switch (i) {
-            case 1:
-                System.out.println("i is 1");
-                break;
-            case 2:
-                System.out.println("i is 2");
-                break;
-            case 3:
-                System.out.println("i is 3");
-                break;
-            default:
-                System.out.println("Default Statement");
-                break;
+        int i=1;
+        // switch (i) {
+        //     case 1:
+        //         System.out.println("i is 1");
+        //         break;
+        //     case 2:
+        //         System.out.println("i is 2");
+        //         break;
+        //     case 3:
+        //         System.out.println("i is 3");
+        //         break;
+        //     default:
+        //         System.out.println("Default Statement");
+        //         break;
+        // }
+
+        while(i<=30)
+        {
+            System.out.println(i);
+            i++;
         }
     }
 }
